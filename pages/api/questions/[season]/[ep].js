9a0108382e4f424fc1ea3aa8427e3af4f4ad3ef2
Larbi Sahli <larbisahli1905@gmail.com>
2020-12-15 10:30:41 +0100

    questions api error handling

--- pages/api/questions/[season]/[ep].js
+++ pages/api/questions/[season]/[ep].js
@@ -1,13 +1,19 @@
-// api/questions/[season]]/[episode]
-
 import _Questions_ from '../../../../data/Questions.json';
 
+function isNumber(n) {
+  return /^-?[\d.]+(?:e-?\d+)?$/.test(n);
+}
+
 export default function ({ query: { season, ep } }, res) {
+  const isValidNumber = !(isNumber(season) && isNumber(ep));
+  if (isValidNumber)
+    return res.status(400).json({ season, ep, error: 'Enter a valid number' });
   const questions = _Questions_[season]?.filter((episode) => {
     return episode.ep === Number(ep);
   })[0];
+  if (questions === undefined)
+    return res
+      .status(404)
+      .json({ season, ep, error: 'The requested resource was not found' });
   res.status(200).json({ season: Number(season), ...questions });
-
-  // handle if ep or season not exist
-  // handle if season and ep are char
 }
